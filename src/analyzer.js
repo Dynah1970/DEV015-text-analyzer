9609bc1df6ed37155a6bebc4d355fc135b303fd5
Dynah Gomez <sinergiadinahgomez19@gmail.com>
2024-05-27 16:56:31 -0600

    cambios generales

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,30 @@
-const analyzer = {  
-  getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
+// TODDO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro text de tipo string //
+    getWordCount: (text) => {
+      const words = text.trim().split(/\s+/).filter(word => word.length > 0);
+      return words.length;
+  }
+  
   getCharacterCount: (text) => {
+      return text.length;
+  }
+  
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-  },
+  
   getCharacterCountExcludingSpaces: (text) => {
+    const characters = text.replace(/\s+/g, '');
+        return characters.length;
+    }
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-  },
+  
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
+  }, 
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
-};
+];
 
 export default analyzer;
