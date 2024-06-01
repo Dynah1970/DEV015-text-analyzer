582a10cc29d287def9f9ea65002b9aa75f144d28
Dynah Gomez <sinergiadinahgomez19@gmail.com>
2024-06-01 00:27:05 -0600

    final de proyecto

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,30 +1,60 @@
-// TODDO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro text de tipo string //
-    getWordCount: (text) => {
-      const words = text.trim().split(/\s+/).filter(word => word.length > 0);
-      return words.length;
-  }
+const analyzer = {  
+
+  //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  getWordCount: (textoFinal) => {
+    const words = textoFinal
+      .trim()
+      .split(/\s+/)
+      .filter((word) => word.length > 0);
+    const wordsCount = words.length;
+    return wordsCount;
+  },
   
-  getCharacterCount: (text) => {
-      return text.length;
-  }
-  
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-  
-  getCharacterCountExcludingSpaces: (text) => {
-    const characters = text.replace(/\s+/g, '');
-        return characters.length;
+  //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+  getCharacterCount: (textoFinal) => {
+    const ccaracteres = textoFinal;
+    const caracteresFinales = ccaracteres.length;
+    return(caracteresFinales);
+  },
+
+  //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+  getCharacterCountExcludingSpaces: (textoFinal) => {
+    const caracteresSEN = textoFinal.replace(/\W/g, "");
+    const caracteresSENR = caracteresSEN.length;
+    return(caracteresSENR); 
+  },
+
+  //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+  getNumberCount: (textoFinal) => {
+    const numberosc = textoFinal.match(/\d+(\.\d+)?(?!\w)/g) || [];
+    const numerosR = numberosc.length;
+    return(numerosR);
+    
+  },
+  //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+  getNumberSum: (textoFinal) => {
+    const sumaNumeros = textoFinal.match(/\d+(\.\d+)?(?!\w)/g);
+    let sumaNumerosR = 0;
+    if (sumaNumeros){
+      for (let i=0; i<sumaNumeros.length; i++){
+        sumaNumerosR=sumaNumerosR + parseFloat(sumaNumeros[i]);
+      }
     }
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-  
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  }, 
-  getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const sumaFinal = Number(sumaNumerosR);
+    return(sumaFinal);
+   
   },
-  getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+
+  //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  getAverageWordLength: (textoFinal) => {  
+    const longitudP = textoFinal.split(" ");
+    let longitudR = 0;
+    for(let i=0; i<longitudP.length; i++){
+      longitudR=longitudR + longitudP[i].length;
+    }
+    return parseFloat((longitudR/longitudP.length).toFixed(2));
+    
   },
-];
+};
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
-import analyzer from './analyzer.js';
+import analyzer from "./analyzer.js";
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+//TODO: escuchar eventos del DOM e invocar  los métodos del objeto analyzer
+const boton = document.getElementById("reset-button");
+const palabras = document.querySelector("li.contadores:nth-child(1)");
+const caracteres = document.querySelector("li.contadores:nth-child(2)");
+const caracteresSE = document.querySelector("li.contadores:nth-child(3)");
+const numeros = document.querySelector("li.contadores:nth-child(4)");
+const suma = document.querySelector("li.contadores:nth-child(5)");
+const longitud = document.querySelector("li.contadores:nth-child(6)");
+const cajaTexto = document.querySelector("textarea");
+
+//metricas
+cajaTexto.addEventListener("input", metricas);
+function metricas() {
+  const textoFinal = cajaTexto.value;
+
+  //contabilizar palabras
+  const wordsCount = analyzer.getWordCount(textoFinal);
+  palabras.innerHTML = "Recuento de palabras:" + wordsCount;
+
+  const charactersCount = analyzer.getCharacterCount(textoFinal);
+  caracteres.innerHTML = "Recuento de caracteres: " + charactersCount;
+  //contabilzar sin  espacios y sin numeros
+  const characterCountEX =
+    analyzer.getCharacterCountExcludingSpaces(textoFinal);
+  caracteresSE.innerHTML =
+    "Recuento de caracteres sin espacios y signos:" + characterCountEX;
+
+  //contabilizar numeros
+  const numberCount = analyzer.getNumberCount(textoFinal);
+  numeros.innerHTML = "Recuento de  numeros:" + numberCount;
+
+  //suma de numeros
+  const sumaFinal = analyzer.getNumberSum(textoFinal);
+  suma.innerHTML = "Suma total de numeros:" + sumaFinal;
+
+  // longitud del texto
+  const averageWord = analyzer.getAverageWordLength(textoFinal);
+  longitud.innerHTML = "Longitud media de palabras;" + averageWord;
+}
+
+boton.addEventListener("click", function () {
+  if (cajaTexto.value === "") {
+    alert("El cuadro esta listo para que escribas en el ");
+  } else {
+    cajaTexto.value = "";
+  }
+  palabras.innerHTML = "Recuento de palabras:0";
+  caracteres.innerHTML = "Recuento de caracteres:0";
+  caracteresSE.innerHTML = "Recuento de caracteres sin espacios y signos:0";
+  numeros.innerHTML = "Recuentos de numeros:0";
+  suma.innerHTML = "Suma total de numeros:0";
+  longitud.innerHTML = "Longitud media de palabras:0";
+});
+
+//TODO: escuchar eventos del DOM e invocar los metodos del objeto 'Analyzer'
